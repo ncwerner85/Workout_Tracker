e143c6d62c2cedea2d19f53b0b7c82ece5519e7e
Nicholas Werner <ncwerner@gmail.com>
2021-03-31 16:56:51 -0600

    creaded routes for api and html

---
+++ routes/apiRoute.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const router = express.Router();
+const Workout = require("../models/Workout.js");
+
+router.get("/api/workouts", (req, res) => {
+  Workout.find({})
+    .sort({ date: -1 })
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.status(400).json(err);
+    });
+});
+
+router.put("/api/workouts/:id", (req, res) => {
+  Workout.findByIdAndUpdate(
+    {
+      _id: req.params.id
+    },
+    {
+      $push: {
+        exercises: req.body
+      }
+    }
+  )
+  .then((error, data) => {
+    if (error) {
+      res.send(error);
+    } else {
+      res.send(data);
+    }
+  });
+});
+
+router.post("/api/workouts", (req, res) => {
+  Workout.create(req.body)
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({})
+    .then(dbWorkout => {
+      res.json(dbWorkout);
+    })
+    .catch(err => {
+      res.json(err);
+    });
+});
+
+module.exports = router;
